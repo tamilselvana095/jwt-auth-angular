71b847c50d6058b4a9cbb725d431b191dcc8d285
tamilselvana <tanilselvana095@gmail.com>
2024-04-17 17:29:15 +0530

    first commit

---
+++ src/app/_services/user-auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService {
+
+  constructor() { }
+
+  public setRoles(roles:[]){
+    localStorage.setItem("roles",JSON.stringify(roles));
+  }
+  public getRoles():[]{
+    const rolesString = localStorage.getItem("roles");
+    if (rolesString !== null) {
+        return JSON.parse(rolesString);
+    } else {
+        return [];
+    }
+  }
+  
+  public setToken(jwtToken:string){
+    localStorage.setItem("jwtToken",jwtToken);
+  }
+
+  public getToken():string | any{
+    return localStorage.getItem("jwtToken");
+  }
+
+  public clear(){
+    localStorage.clear();
+  }
+
+  public isLoggedIn(){
+    return this.getRoles() && this.getToken();
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../_services/user.service';
+import { UserAuthService } from '../_services/user-auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private userService: UserService, 
+              private userAuthService: UserAuthService,
+              private router:Router) { }
+
+  login(loginForm: NgForm) {
+
+    this.userService.login(loginForm.value).subscribe(
+      (response:any) => {
+        console.log(response.jwtToken);
+        console.log(response.user.role);
+
+        this.userAuthService.setRoles(response.user.role);
+        this.userAuthService.setToken(response.jwtToken);
+
+        //console.log(response);
+        const role=response.user.role[0].roleName;
+        if(role === 'Admin'){
+          this.router.navigate(['/admin'])
+        }else{
+          this.router.navigate(['/user'])
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+}
